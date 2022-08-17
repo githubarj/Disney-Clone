7364c738538131944a36d81ab0c145488a6e6de4
Richard J Githuba <85483196+githubarj@users.noreply.github.com>
2022-08-17 20:20:41 +0300

    slick-carousel

--- src/Componets/ImgSlider.jsx
+++ src/Componets/ImgSlider.jsx
@@ -54,11 +54,10 @@ const Carousel = styled(Slider)`
     }
   }
 
-
   ul li button {
     &:before {
-        font-size: 10px;
-        color: rgb(150, 158, 171);
+      font-size: 10px;
+      color: rgb(150, 158, 171);
     }
   }
   li.slick-active button:before {
@@ -67,16 +66,39 @@ const Carousel = styled(Slider)`
   .slick-list {
     overflow: initial;
   }
-  .slick-prev{
+  .slick-prev {
     left: -75px;
-
   }
-  .slick-next{
+  .slick-next {
     right: -75px;
   }
-
 `;
 
-const Wrap = styled.div``
+const Wrap = styled.div`
+  border-radius: 4px;
+  cursor: pointer;
+  position: relative;
+
+  a {
+    border-radius: 4px;
+    box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+      rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+    cursor: pointer;
+    display: block;
+    position: relative;
+    padding: 4px;
+
+    img {
+        width: 100%;
+        height: 100%;
+    }
+
+    &:hover {
+        padding: 0;
+        border: 4px solid rgba(249, 249, 249, 0.8);
+        transition-duration: 300ms;
+    }
+  }
+`;
 
 export default ImgSlider;
