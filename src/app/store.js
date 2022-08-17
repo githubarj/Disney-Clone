7653178c08024d7b222bc5e45967bba84ca6e507
Richard J Githuba <85483196+githubarj@users.noreply.github.com>
2022-08-17 03:02:15 +0300

    redux

---
+++ src/app/store.js
@@ -0,0 +1,6 @@
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+
+export default configureStore({
+  reducer: {},
+  middleware: getDefaultMiddleware({ serializableCheck: false }),
+});
