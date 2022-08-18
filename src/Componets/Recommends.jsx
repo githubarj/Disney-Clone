643c0380954ec4470e4cda69666f40e205d07609
Richard J Githuba <85483196+githubarj@users.noreply.github.com>
2022-08-18 05:41:51 +0300

    home page done

--- src/Componets/Recommends.jsx
+++ src/Componets/Recommends.jsx
@@ -1,34 +1,23 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectRecommend } from "../features/movie/movieSlice";
 
 const Recommends = (props) => {
+  const movies = useSelector(selectRecommend);
   return (
     <Container>
-      <h4>Recommended For You</h4>
+      <h4>Recommended for You</h4>
       <Content>
-        <Wrap>
-          <Link to="/">
-            <img src="/images/viewers-disney.png" alt="" />
-          </Link>
-        </Wrap>
-
-        <Wrap>
-          <Link to="/">
-            <img src="/images/viewers-disney.png" alt="" />
-          </Link>
-        </Wrap>
-
-        <Wrap>
-          <Link to="/">
-            <img src="/images/viewers-disney.png" alt="" />
-          </Link>
-        </Wrap>
-
-        <Wrap>
-          <Link to="/">
-            <img src="/images/viewers-disney.png" alt="" />
-          </Link>
-        </Wrap>
+        {movies &&
+          movies.map((movie, key) => (
+            <Wrap key={key}>
+              {movie.id}
+              <Link to={`/detail/` + movie.id}>
+                <img src={movie.cardImg} alt={movie.title} />
+              </Link>
+            </Wrap>
+          ))}
       </Content>
     </Container>
   );
@@ -56,8 +45,9 @@ const Wrap = styled.div`
     rgb(0 0 0 / 73%) 0px 16px 10px -10px;
   cursor: pointer;
   overflow: hidden;
+  position: relative;
   transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
-  border: solid rgba(249, 249, 249, 0.1);
+  border: 3px solid rgba(249, 249, 249, 0.1);
 
   img {
     inset: 0px;
@@ -65,7 +55,7 @@ const Wrap = styled.div`
     height: 100%;
     object-fit: cover;
     opacity: 1;
-    /* position: absolute; */
+    position: absolute;
     transition: opacity 500ms ease-in-out 0s;
     width: 100%;
     z-index: 1;
@@ -76,7 +66,7 @@ const Wrap = styled.div`
     box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
       rgb(0 0 0 / 72%) 0px 30px 22px -10px;
     transform: scale(1.05);
-    border-color: rgba(249,249,249,0.8);
+    border-color: rgba(249, 249, 249, 0.8);
   }
 `;
 
